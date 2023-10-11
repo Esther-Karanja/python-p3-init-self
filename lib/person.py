6db8133e8d275d118c6f1d8046b213bd56609556
Esther Karanja <est.karanja@gmail.com>
2023-10-11 15:47:28 -0700

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name =name
+
+    # def __str__(self):
+    #     output=f"{self.name}"
+    #     return output
+
+
+p1 = Person("Essy")
+print(p1)
